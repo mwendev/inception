fe00c943f9592ba334050aa027ba146a31414271
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-09 16:59:34 +0200

    wordpress site 0.1

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -7,7 +7,7 @@ done
 
 if [ ! -f "/var/www/html/index.html" ]; then
 
-    mv /tmp/greppo/* /var/www/html
+    mv /tmp/greppo/* /var/www/html/index.html
 
 	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='localhost' --dbcharset="utf8" --dbcollate="utf8_general_ci" --config-file=/var/www/html/wp-config.php
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=/var/www/html
