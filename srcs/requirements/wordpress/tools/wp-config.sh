6e01d39d2b56b1a63306570ff326a42a0d1d9ea8
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-09 20:23:36 +0200

    wordpress site 0.2.1

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #wait for mysql
-while ! mariadb -h'localhost' -u$MARIADB_USER_NAME -p$MARIADB_USER_PWD $MARIADB_DB_NAME &>/dev/null; do
+while ! mariadb -h'localhost:3306' -u$MARIADB_USER_NAME -p$MARIADB_USER_PWD $MARIADB_DB_NAME &>/dev/null; do
     sleep 3
 done
 
@@ -9,7 +9,7 @@ if [ ! -f "/var/www/html/index.html" ]; then
 
     mv /tmp/greppo/* /var/www/html/index.html
 
-	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='localhost' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
+	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='localhost:3306' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
 	#--config-file=/var/www/html/wp-config.php
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	# --path=/var/www/html/greppo
