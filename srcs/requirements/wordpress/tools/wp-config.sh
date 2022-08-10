c59314870a853d9962777c63d1ca62abbe0c7fda
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-10 12:14:43 +0200

    wordpress site 0.2.6

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -7,14 +7,13 @@ done
 
 if [ ! -f "/var/www/html/index.html" ]; then
 
-	mkdir -p /var/www/html/greppo
-    mv /tmp/greppo/* /var/www/html/greppo
+	# rm -f /var/www/html/index.html
+	# mv /tmp/greppo/* /var/www/html
 
 	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='localhost:3306' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
-	#--config-file=/var/www/html/wp-config.php
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	# --path=/var/www/html/greppo
-	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
+    wp theme install inspiro --activate --allow-root
 fi
 
 /usr/sbin/php-fpm -F -R
