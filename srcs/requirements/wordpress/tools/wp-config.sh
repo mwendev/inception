9a580d4b9db5beb8aac2e1bc4e62e871e20f0f7e
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 01:46:43 +0200

    1.7

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #wait for mysql
-while ! mariadb -h'localhost' -u$MARIADB_USER_NAME -p$MARIADB_USER_PWD $MARIADB_DB_NAME &>/dev/null; do
+while ! mariadb -h'mariadb' -u$MARIADB_USER_NAME -p$MARIADB_USER_PWD $MARIADB_DB_NAME &>/dev/null; do
 	sleep 3
 done
 
@@ -14,7 +14,7 @@ if [ ! -f "/var/www/html/index.html" ]; then
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
     wp core download --allow-root
-	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='localhost' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
+	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='mariadb' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
     wp theme install inspiro --activate --allow-root
