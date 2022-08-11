f8c352b92ec47a06aece65a5ec84e89b1f3220b5
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 16:12:57 +0200

    2.3.5

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -5,12 +5,12 @@
 # 	chown -R mysql:mysql /run/mysqld
 # fi
 
-# if [ -d "/var/lib/mysql" ]; then
+if [ -d "/var/lib/mysql" ]; then
 	
-	# chown -R mysql:mysql /var/lib/mysql
+	chown -R mysql:mysql /var/lib/mysql
 
-	#init database
-	# mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+	init database
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
 	# run init.sql
 	# /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
@@ -24,12 +24,12 @@
 # FLUSH PRIVILEGES;
 # MYSQL_SCRIPT
 # 	echo "hi"
-# fi
+fi
 
 # allow remote connections
-# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 # exec /usr/bin/mysqld --user=mysql
-# exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
+exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
 
