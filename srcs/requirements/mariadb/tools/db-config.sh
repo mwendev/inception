75e8b3b114fc7bbc99a16a78bed3b351e403e8e6
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 15:28:52 +0200

    2.3

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -10,19 +10,17 @@ if [ -d "/var/lib/mysql" ]; then
 	chown -R mysql:mysql /var/lib/mysql
 
 	#init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+	# mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
 	# run init.sql
-	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
-	rm -f /tmp/init_db.sql
+	# /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
+	# rm -f /tmp/init_db.sql
 fi
 
 # allow remote connections
 # sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 # sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql
-# exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
+# exec /usr/bin/mysqld --user=mysql
+exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
 
