5ba142ee1003f0afc933aa86c66f187389e8e678
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 18:32:04 +0200

    3.2.4

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -15,10 +15,10 @@ if [ -f "/var/www/html/index.html" ]; then
     wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
-    wp core download --allow-root
+    wp core download --allow-root --force
 	echo "yo"
     wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --skip-email
 	echo "who wants php"
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
  	echo "is it you"
