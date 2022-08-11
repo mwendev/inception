52b5edcf79b5968c52123dc07152230576130772
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 15:55:39 +0200

    2.3.3

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -30,6 +30,6 @@
 # sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 # sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql
-# exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
+# exec /usr/bin/mysqld --user=mysql
+exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
 
