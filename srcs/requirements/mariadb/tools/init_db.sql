7ecb0737d4a55fd3fbf58d8a460c8c9b434f6eb4
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 19:49:15 +0200

    ISSUE: wordpress 1

--- srcs/requirements/mariadb/tools/init_db.sql
+++ srcs/requirements/mariadb/tools/init_db.sql
@@ -4,6 +4,7 @@ FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS wordpress;
 
 CREATE USER 'mwen'@'localhost' IDENTIFIED BY 'mwen42';
+CREATE USER 'mwen'@'wordpress.inception' IDENTIFIED BY 'mwen42';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'mwen'@'%';
 
 FLUSH PRIVILEGES;
