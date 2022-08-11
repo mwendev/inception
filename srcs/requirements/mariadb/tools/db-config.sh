dd96f64adad7b7ca40da9304c2dea1df4ab9d6f1
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 19:10:41 +0200

    ISSUE: del vol mysql config 3

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -13,16 +13,16 @@ if [ -d "/var/lib/mysql" ]; then
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
 	# run init.sql
-	# /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
+	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
 	# rm -f /tmp/init_db.sql
 
-	mysql -u root <<MYSQL_SCRIPT
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS $MARIADB_DB_NAME;
-CREATE USER IF NOT EXISTS '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PWD';
-GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+# 	mysql -u root <<MYSQL_SCRIPT
+# FLUSH PRIVILEGES;
+# CREATE DATABASE IF NOT EXISTS $MARIADB_DB_NAME;
+# CREATE USER IF NOT EXISTS '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PWD';
+# GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
+# FLUSH PRIVILEGES;
+# MYSQL_SCRIPT
 # 	echo "hi"
 fi
 
@@ -30,6 +30,6 @@ fi
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-# exec /usr/bin/mysqld --user=mysql
 exec /usr/bin/mysqld --user=mysql
+# exec /usr/bin/mysqld --user=mysql --init-file=/tmp/init_db.sql
 
