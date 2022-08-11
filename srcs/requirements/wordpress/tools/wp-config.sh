a7b16cbf7c430c07ddcb800c1ee0df19ef3e9fd7
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 18:08:03 +0200

    3.1.9

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -3,11 +3,11 @@
 #test mysql
 while ! mariadb -h'mariadb' -u'mwen' -p'mwen42' wordpress &>/dev/null; do
 	sleep 3
-	echo "mm?"
 done
 
 if [ -f "/var/www/html/index.html" ]; then
 
+	rm -f /var/www/html/index.html
 	mv /tmp/index.html /var/www/html/index.html
 
     # adminer
@@ -15,7 +15,6 @@ if [ -f "/var/www/html/index.html" ]; then
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
     wp core download --allow-root
-	wp config create --allow-root --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER_NAME --dbpass=$MARIADB_USER_PWD --dbhost='mariadb' --dbcharset="utf8" --dbcollate="utf8_general_ci" 
 	echo "yo"
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
