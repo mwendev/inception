d112d504e80a1523bf1d10335156e97f6ea7482a
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 13:57:49 +0200

    2.2.1

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -26,4 +26,4 @@ echo "3"
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql
+exec /usr/bin/mysqld -u $MARIADB_USER_NAME -p $MARIADB_USER_PWD $MARIADB_DB_NAME
