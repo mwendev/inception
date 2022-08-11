b6577f9f8f27f09a0d9f305ee739f02b51813aec
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 18:26:25 +0200

    3.2.2

--- srcs/requirements/wordpress/tools/wp-config.sh
+++ srcs/requirements/wordpress/tools/wp-config.sh
@@ -7,20 +7,17 @@ done
 
 if [ -f "/var/www/html/index.html" ]; then
 
-	find . -name wp-config.php
 	mv /tmp/index.html /var/www/html/index.html
 	echo "huh"
 
 
     # adminer
     wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
-	echo "say what"
-
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
-	echo "what"
 
     wp core download --allow-root
 	echo "yo"
+    wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	echo "who wants php"
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
