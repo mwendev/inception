ec7fc9a79dc7cd348c7b23aa00d312bb3531abcd
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-11 02:08:27 +0200

    1.9

--- srcs/requirements/mariadb/tools/db-config.sh
+++ srcs/requirements/mariadb/tools/db-config.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+# if [ ! -d "/run/mysqld" ]; then
+# 	mkdir -p /run/mysqld
+# 	chown -R mysql:mysql /run/mysqld
+# fi
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
 	
-	chown -R mysql:mysql /var/lib/mysql
+# 	chown -R mysql:mysql /var/lib/mysql
 
-	# init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+# 	# init database
+# 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
+# 	tfile=`mktemp`
+# 	if [ ! -f "$tfile" ]; then
+# 		return 1
+# 	fi
 
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
-	cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PWD';
-CREATE DATABASE $MARIADB_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED by '$MARIADB_USER_PWD';
-GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
-FLUSH PRIVILEGES;
-EOF
-	# run init.sql
-	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-	rm -f $tfile
-fi
+# 	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+# 	cat << EOF > $tfile
+# USE mysql;
+# FLUSH PRIVILEGES;
+# DELETE FROM	mysql.user WHERE User='';
+# DROP DATABASE test;
+# DELETE FROM mysql.db WHERE Db='test';
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PWD';
+# CREATE DATABASE $MARIADB_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED by '$MARIADB_USER_PWD';
+# GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
+# FLUSH PRIVILEGES;
+# EOF
+# 	# run init.sql
+# 	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+# 	rm -f $tfile
+# fi
 
-# allow remote connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+# # allow remote connections
+# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql --console
+# exec /usr/bin/mysqld --user=mysql --console
+exec mysqld --user=mysql --init-file=/tmp/dbinit.sql
