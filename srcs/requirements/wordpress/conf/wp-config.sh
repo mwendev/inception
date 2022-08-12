ca8eaaf48d42abb979a31128d86e819deef02103
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-12 17:56:50 +0200

    4.3

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -17,6 +17,17 @@ if [ ! -f "/var/www/html/index.html" ]; then
     wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email='mwen@gmail.com' --skip-email
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
+    wp theme install inspiro --activate --allow-root
+
+    # enable redis cache
+    sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"      wp-config.php
+    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
+    sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
+    sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
+    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
+
 fi
+
+wp redis enable --allow-root
 
 /usr/sbin/php-fpm7 -F -R
