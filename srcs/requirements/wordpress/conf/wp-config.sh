95db37812828c6213ff3db578c1d009a5df4eae1
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-12 15:06:41 +0200

    4.2

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -7,8 +7,6 @@ done
 
 if [ ! -f "/var/www/html/index.html" ]; then
 
-	mv /tmp/index.html /var/www/html/index.html
-
     # adminer
     wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
