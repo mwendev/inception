cd839e16a5e0ba5260667d03e27a94dc4420a56c
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-12 19:32:25 +0200

    4.3.5

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -15,8 +15,8 @@ if [ ! -f "/var/www/html/index.html" ]; then
 
     wp core download --allow-root --force
     wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email='mwen@gmail.com' --skip-email
-	wp user create --allow-root mwen mwen@gmail.com --user_pass=mwen42 --role=author
+	wp core install --allow-root --url=localhost --title=inception --admin_user=wpmwen --admin_password=iamadmin --admin_email=mwen@gmail.com --skip-email
+	wp user create --allow-root mwen mwen@gmail.com --user_pass=imuser --role=author
     wp theme install inspiro --activate --allow-root
 
     # enable redis cache
