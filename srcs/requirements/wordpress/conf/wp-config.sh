1cce6127427645c6ac52ffd42fa107f3ae2448a8
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-08-13 08:57:15 +0200

    4.3.7

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -9,17 +9,19 @@ if [ ! -f "/var/www/html/index.html" ]; then
 
 	mv /tmp/greppo/* /var/www/html
 
-    # adminer
-    wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
-    wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
+	# adminer
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
+	wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
-    wp core download --allow-root --force
-    wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --allow-root --url=localhost --title=inception --admin_user=wpmwen --admin_password='iamadmin' --admin_email=wpmwen@gmail.com --skip-email
-	wp user create --allow-root mwen mwen@gmail.com --user_pass='imuser' --role=author
-    wp theme install inspiro --activate --allow-root
+	wp core download --allow-root --force
+	wp config create --dbname=wordpress --dbuser=mwen --dbpass=mwen42 --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp core install --allow-root --url=localhost --title=inception --admin_user=wpmwen --admin_password="Iamadmin123" --admin_email=wpmwen@gmail.com --skip-email
+	wp user create --allow-root $WP_USER_LOGIN mwen@gmail.com --user_pass="Imuser123" --role=author
+	wp user update wpmwen --user_pass=Iamadmin123;
+	wp user update mwen --user_pass=Imuser123;
+	wp theme install inspiro --activate --allow-root
 
-    # enable redis cache
+	# enable redis cache
 	sed -i "40i define( 'WP_REDIS_HOST', 'redis' );"			wp-config.php
 	sed -i "41i define( 'WP_REDIS_PORT', 6379 );"				wp-config.php
 	sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"				wp-config.php
